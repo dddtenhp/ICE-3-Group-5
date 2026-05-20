7468d76216351ee50c6fe6379770fa5ec7a1133c
dddtenhp <pham43@sheridancollege.ca>
2026-05-20 11:42:02 -0400

    person 3 done

--- src/test/MakeChangeTest.java
+++ src/test/MakeChangeTest.java
@@ -13,6 +13,7 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import main.BadChangeException;
@@ -20,6 +21,7 @@ import main.MakeChange;
 
 public class MakeChangeTest {
 	
+	 // @Disabled("Already tested by previous teammate")
 	 @Test
 	    void testMakeChange40Cents() throws BadChangeException {
 
@@ -27,5 +29,22 @@ public class MakeChangeTest {
 	                "40 cents requires 1 quarter, 1 dime, 1 nickel",
 	                MakeChange.makeChange(40)
 	        );
-	    }
+	 }
+	 
+	 @Test
+	 void testMakeChange19Cents() throws BadChangeException {
+	     assertEquals(
+	             "19 cents requires 2 dimes",
+	             MakeChange.makeChange(19)
+	     );
+	 }
+
+	 @Test
+	 void testMakeChange92Cents() throws BadChangeException {
+	     assertEquals(
+	             "92 cents requires 3 quarters, 1 dime, 1 nickel",
+	             MakeChange.makeChange(92)
+	     );
+	 }
+	 
 }
