493cfff3747e8805052f9bfadf17e214a2d9f34d
dddtenhp <pham43@sheridancollege.ca>
2026-05-20 07:16:11 -0400

    person 2: write minimal code to pass Test 1

--- src/main/MakeChange.java
+++ src/main/MakeChange.java
@@ -10,9 +10,18 @@ This class calculates change using quarters, dimes, and nickels.
 package main;
 
 public class MakeChange {
-	public static String makeChange(int cents)
-            throws BadChangeException {
-
-        return "";
+	public static String makeChange(int cents) throws BadChangeException {
+		
+		int original = cents;
+		int quarters = cents / 25;
+		cents %= 25;
+		int dimes = cents / 10;
+		cents %= 10;
+		int nickels = cents / 5;
+        return String.format("%d cents requires %d quarter, %d dime, %d nickel",
+                original,
+                quarters,
+                dimes,
+                nickels);
     }
 }
